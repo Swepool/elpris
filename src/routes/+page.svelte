2e8471a9487ae00310f875efbef242d1773e18dd
Lukas <36674091+Swepool@users.noreply.github.com>
2022-09-18 15:37:20 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,60 +5,116 @@
     export let data
 
     let currentPrice = data.currentPrice
+    let highestPrice = (data.priceTable.sort((a,b)=>a.price-b.price).reverse()[0]).price;
+    let lowestPrice = (data.priceTable.sort((a,b)=>b.price-a.price).reverse()[0]).price;
 </script>
 
-<div class="grid container">
-    <div class="card">
-        <h3>Current price:</h3>
-        <h2>{currentPrice} € / kWh</h2>
+<div class="container">
+    <h2 class="title">Price per kWh</h2>
+    <div class="grid">
+        <div class="card">
+            <div>
+                <h3>Current price:</h3>
+                <h3>( kWh )</h3>
+            </div>
+            <h2>{currentPrice} €</h2>
+        </div>
+        <div class="card">
+            <div>
+                <h3>Highest price:</h3>
+                <h3>( kWh )</h3>
+            </div>
+            <h2>{highestPrice} €</h2>
+        </div>
+        <div class="card">
+            <div>
+                <h3>Lowest price:</h3>
+                <h3>( kWh )</h3>
+            </div>
+            <h2>{lowestPrice} €</h2>
+        </div>
+        <div class="card">
+            <div>
+                <h3>Lowest price:</h3>
+                <h3>( kWh )</h3>
+            </div>
+            <h2>{lowestPrice} €</h2>
+        </div>
     </div>
-    <div class="card">
-        <h3>Shower (10min):</h3>
-        <h2>{shower(currentPrice, 10)} €</h2>
-    </div>
-    <div class="card">
-        <h3>Dishwasher:</h3>
-        <h2>{dishwasher(currentPrice)} €</h2>
-    </div>
-    <div class="card">
-        <h3>Air Condition (1hour):</h3>
-        <h2>{chill(currentPrice)} €</h2>
+</div>
+<div class="container">
+    <h2 class="title">Usage</h2>
+    <div class="grid">
+        <div class="card">
+            <div>
+                <h3>Air Condition:</h3>
+                <h3>( 1 hour )</h3>
+            </div>
+            <h2>{chill(currentPrice)} €</h2>
+        </div>
+        <div class="card">
+            <div>
+                <h3>Shower:</h3>
+                <h3>( 10 min )</h3>
+            </div>
+            <h2>{shower(currentPrice, 10)} €</h2>
+        </div>
+        <div class="card">
+            <h3>Dishwasher:</h3>
+            <h2>{dishwasher(currentPrice)} €</h2>
+        </div>
+        <div class="card">
+            <h3>Dishwasher:</h3>
+            <h2>{dishwasher(currentPrice)} €</h2>
+        </div>
     </div>
 </div>
 
 
-
 <style lang="scss">
-    .grid {
-        display: grid;
-        grid-gap: 1rem;
-        grid-template-columns: repeat(12, minmax(0, 1fr));
+
+  .title {
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+    font-weight: 400;
+  }
+  .grid {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(12, minmax(0, 1fr));
+    margin-bottom: 1rem;
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    border-radius: 10px;
+    background-color: var(--card-background);
+    grid-column: span 6 / span 6;
+    border: var(--card-border);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.05),
+    0 6px 6px rgba(0, 0, 0, 0.10);
+
+    div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
     }
 
+    h3 {
+      font-weight: 200;
+    }
+
+    h2 {
+      font-weight: 400;
+    }
+  }
+
+  @media screen and (max-width: 568px) {
     .card {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        padding: 1rem;
-        border-radius: 10px;
-        background-color: var(--card-background);
-        grid-column: span 6 / span 6;
-        border: var(--card-border);
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.05),
-        0 6px 6px rgba(0, 0, 0, 0.10);
-
-      h3 {
-        font-weight: 200;
-      }
-
-      h2 {
-        font-weight: 400;
-      }
+      grid-column: span 12 / span 12;
     }
-
-    @media screen and (max-width: 568px) {
-        .card {
-            grid-column: span 12 / span 12;
-        }
-    }
+  }
 </style>
